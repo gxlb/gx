35579a3da558181990a42ce6a0bc5cc4130c7c2e
Ally Dale <vipally@163.com>
2016-09-20 22:46:17 +0800

    add rdtsc

---
+++ time/rdtsc.go
@@ -0,0 +1,23 @@
+//Package time implement some time function such as RDTSC() from C
+package time
+
+/*
+#include<time.h>
+unsigned long long rdtsc(void)
+{
+	unsigned long long ullCPUTick = 0;
+	__asm__ __volatile__("rdtsc" : "=A" (ullCPUTick) : );
+	return ullCPUTick;
+}
+*/
+import "C"
+
+//call rdtsc from C
+func RDTSC() uint64 {
+	return uint64(C.rdtsc())
+}
+
+//call clock() from C
+func Clock() uint64 {
+	return uint64(C.clock())
+}
