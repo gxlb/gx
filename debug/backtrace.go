21add7540a7358ae28b251735b5ba38a86eb272b
Ally Dale <vipally@163.com>
2016-09-21 14:40:42 +0800

    debug lib

--- debug/backtrace.go
+++ debug/backtrace.go
@@ -7,59 +7,59 @@ import (
 	"runtime"
 )
 
-type t_fmt uint32
+type tFmt uint32
 
 const (
-	BT_ME     = 2
-	BT_FATHER = BT_ME + 1
+	BtMe     = 2
+	BtFather = BtMe + 1
 )
 
 const (
-	max_bt_list_len       = 50
-	fmt_fileline    t_fmt = iota + 1
-	fmt_func
-	fmt_all
+	maxBtListLen      = 50
+	fmtFileLine  tFmt = iota + 1
+	fmtFunc
+	fmtAll
 )
 const (
-	str_caller    = "Caller:"
-	str_cur_frame = ""
+	strCaller   = "Caller:"
+	strCurFrame = ""
 )
 
 //tell me current call frame
 func Bt() string {
-	return CallStackInfo(BT_ME, fmt_all, str_cur_frame)
+	return CallStackInfo(BtMe, fmtAll, strCurFrame)
 }
 
 //tell me current fileline
 func BtFileLine() string {
-	return CallStackInfo(BT_ME, fmt_fileline, str_cur_frame)
+	return CallStackInfo(BtMe, fmtFileLine, strCurFrame)
 }
 
 //tell me current func
 func BtFunc() string {
-	return CallStackInfo(BT_ME, fmt_func, str_cur_frame)
+	return CallStackInfo(BtMe, fmtFunc, strCurFrame)
 }
 
 //tell me caller call frame
-func Caller(_depth int) string {
-	return CallStackInfo(BT_FATHER+_depth, fmt_all, str_caller)
+func Caller(depth int) string {
+	return CallStackInfo(BtFather+depth, fmtAll, strCaller)
 }
 
 //tell me caller fileline
-func CallerFileLine(_depth int) string {
-	return CallStackInfo(BT_FATHER+_depth, fmt_fileline, str_caller)
+func CallerFileLine(depth int) string {
+	return CallStackInfo(BtFather+depth, fmtFileLine, strCaller)
 }
 
 //tell me caller func
-func CallerFunc(_depth int) string {
-	return CallStackInfo(BT_FATHER+_depth, fmt_func, str_caller)
+func CallerFunc(depth int) string {
+	return CallStackInfo(BtFather+depth, fmtFunc, strCaller)
 }
 
 //use a fast-way to build string
 func Bts() (bts string) {
 	var buf bytes.Buffer
-	var __pcs [max_bt_list_len]uintptr
-	n := runtime.Callers(BT_ME, __pcs[0:])
+	var __pcs [maxBtListLen]uintptr
+	n := runtime.Callers(BtMe, __pcs[0:])
 	for i := 0; i < n; i++ {
 		_pc := __pcs[i]
 		_f := runtime.FuncForPC(_pc)
@@ -72,18 +72,18 @@ func Bts() (bts string) {
 	return buf.String()
 }
 
-func CallStackInfo(nDepth int, _fmt t_fmt, strhead string) (_info string) {
+func CallStackInfo(nDepth int, _fmt tFmt, strhead string) (_info string) {
 	__pc, __file, __line, __ok := runtime.Caller(nDepth)
 
 	if __ok {
 		__f := runtime.FuncForPC(__pc)
 		switch _fmt {
-		case fmt_fileline:
+		case fmtFileLine:
 			_info = fmt.Sprintf("%s FileLine{%s : %d}",
 				strhead, __file, __line)
-		case fmt_func:
+		case fmtFunc:
 			_info = fmt.Sprintf("%s Func{%s}", strhead, __f.Name())
-		case fmt_all:
+		case fmtAll:
 			fallthrough
 		default:
 			_info = fmt.Sprintf("%s Func{%s} FileLine{%s : %d} pc{%0X}",
@@ -94,7 +94,7 @@ func CallStackInfo(nDepth int, _fmt t_fmt, strhead string) (_info string) {
 }
 
 func CallStackList(nDepth int) []string {
-	var __pcs [max_bt_list_len]uintptr
+	var __pcs [maxBtListLen]uintptr
 	n := runtime.Callers(nDepth, __pcs[0:])
 	cs := make([]string, 0, n)
 	for i := 0; i < n; i++ {
