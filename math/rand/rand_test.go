e0dddc56f66aef388d8fc6876b513e2ea23f51d1
Ally Dale <vipally@163.com>
2016-09-26 15:26:03 +0800

    auto run gogp tool

--- math/rand/rand_test.go
+++ math/rand/rand_test.go
@@ -1,16 +1,16 @@
 package rand
 
 import (
-	"os"
-	"testing"
+	//	"os"
+	//	"testing"
 
-	"github.com/vipally/gogp"
+	_ "github.com/vipally/gogp" //auto run gogp tool in test process
 )
 
-func TestCallGogp(t *testing.T) {
-	if dir, err := os.Getwd(); err == nil {
-		gogp.Work(dir)
-	} else {
-		panic(err)
-	}
-}
+//func TestCallGogp(t *testing.T) {
+//	if dir, err := os.Getwd(); err == nil {
+//		gogp.Work(dir)
+//	} else {
+//		panic(err)
+//	}
+//}
