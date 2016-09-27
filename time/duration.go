1207d4bd163c2b234e094ee91280b81e64fd57af
Ally Dale <vipally@163.com>
2016-09-28 07:06:46 +0800

    expand std.time.Duration and test cases

--- time/duration.go
+++ time/duration.go
@@ -1,29 +1,93 @@
+// to extend std.time.Duration
+// and hide std.time for clients
+
 package time
 
 import (
+	"fmt"
 	"time"
 )
 
+// compile error: cannot define new methods on non-local type time.Duration
+//func (d time.Duration) Days() float64 {
+//	return float64(d) / float64(Day)
+//}
+
+//Duration is expand type for time.Duration
 type Duration time.Duration
 
 const (
+	Day  = 24 * Hour //expand const
+	Week = 7 * Day   //expand const
+
+	////////////////////////////////////////////////////////
+	//re-export std.Duration.consts
+	//THIS MAKE MY EXTEND CODE UGLY
+
 	Nanosecond  Duration = Duration(time.Nanosecond)
 	Microsecond          = Duration(time.Microsecond)
 	Millisecond          = Duration(time.Millisecond)
 	Second               = Duration(time.Second)
 	Minute               = Duration(time.Minute)
 	Hour                 = Duration(time.Hour)
-	Day                  = 24 * Hour
-	Week                 = 7 * Day
+	////////////////////////////////////////////////////////
 )
 
-func (d Duration) T() time.Duration {
+//std
+func (d Duration) Std() time.Duration {
 	return time.Duration(d)
 }
+
+//expand method
 func (d Duration) Days() float64 {
 	return float64(d) / float64(Day)
 }
 
+//expand method
 func (d Duration) Weeks() float64 {
 	return float64(d) / float64(Week)
 }
+
+//expand std.Duration.String
+func (d Duration) String() string {
+	if d < Day {
+		return d.Std().String()
+	} else {
+		days := int(d / Day)
+		left := d % Day
+		return fmt.Sprintf("%dd%s", days, left.Std().String())
+	}
+	return ""
+}
+
+////////////////////////////////////////////////////////
+//re-export std.Duration.methods
+//THIS MAKE MY EXTEND CODE UGLY
+
+//re-export std.time.ParseDuration
+func ParseDuration(s string) (Duration, error) {
+	d, err := time.ParseDuration(s)
+	return Duration(d), err
+}
+
+//re-export std.time.Since
+func Since(t time.Time) Duration {
+	return Duration(time.Since(t))
+}
+
+//re-export std.Duration.Seconds
+func (d Duration) Seconds() float64 {
+	return d.Std().Seconds()
+}
+
+//re-export std.Duration.Minutes
+func (d Duration) Minutes() float64 {
+	return d.Std().Minutes()
+}
+
+//re-export std.Duration.Hours
+func (d Duration) Hours() float64 {
+	return d.Std().Hours()
+}
+
+////////////////////////////////////////////////////////
--- time/duration_test.go
+++ time/duration_test.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"testing"
 
+	"time" //github.com/vipally/gx/time"
+
 	xtime "github.com/vipally/gx/time"
 )
 
+func TestStdDuration(t *testing.T) {
+	var d time.Duration = time.Hour*25 + time.Minute
+	fmt.Printf("DurationValue:%d, String:%s, Hours:%f\n",
+		int64(d), d, d.Hours())
+
+	//import("github.com/vipally/gx/time"):
+	//	DurationValue:90060000000000, String:1d1h1m0s, Hours:25.016667
+
+	//import("time"):
+	//	DurationValue:90060000000000, String:25h1m0s, Hours:25.016667
+}
+
 func TestDuration(t *testing.T) {
-	var d xtime.Duration = xtime.Day + xtime.Hour
-	fmt.Println("Seconds", d, d.T().Seconds())
-	fmt.Println("Days", d, d.Days())
-	fmt.Println("Weeks", d, d.Weeks())
+	var ds = []xtime.Duration{xtime.Day + xtime.Hour*12, xtime.Hour*12 + xtime.Minute}
+	for _, d := range ds {
+		fmt.Printf("DurationValue:%d, std:%s, expand:%s, Days:%f, Hours:%f\n",
+			int64(d), d.Std(), d, d.Days(), d.Hours())
+	}
+
+	//DurationValue:129600000000000, std:36h0m0s, expand:1d12h0m0s, Days:1.500000, Hours:36.000000
+	//DurationValue:43260000000000, std:12h1m0s, expand:12h1m0s, Days:0.500694, Hours:12.016667
 }
