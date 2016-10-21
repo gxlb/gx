d6503d92257444666b326935fa636e9de9619e74
Ally Dale <vipally@163.com>
2016-10-22 00:00:39 +0800

    heap

--- stl/gp/heap.go
+++ stl/gp/heap.go
@@ -40,55 +40,110 @@ func (me GOGPHeapElem) Show() string {
 
 //stack object
 type GOGPHeapNamePrefixHeap struct {
-	d      []GOGPHeapElem
-	limitN int
-	maxTop bool
+	d      []GOGPHeapElem //data buffer
+	limitN int            //if limitN>0, heap size must<=limitN
+	maxTop bool           //if top is max value
+}
+
+//push heap value
+func (this *GOGPHeapNamePrefixHeap) PushHeap(d []GOGPHeapElem, v GOGPHeapElem) []GOGPHeapElem {
+	d = append(d, v)
+	idx := len(d) - 1
+	parent := this.parent(idx)
+	for ; idx > 0; idx, parent = parent, this.parent(parent) {
+		if this.cmpV(d[idx], d[parent]) {
+			d[idx], d[parent] = d[parent], d[idx]
+		} else {
+			break
+		}
+	}
+	return d
+}
+
+//pop heap top
+func (this *GOGPHeapNamePrefixHeap) PopHeap(d []GOGPHeapElem) (h []GOGPHeapElem, top GOGPHeapElem, ok bool) {
+	l := len(d)
+	if ok = l > 0; ok {
+		top = d[0]
+		if l > 1 {
+			d[0], d[l-1] = d[l-1], d[0]
+			this.adjust(d[:l-1], 0, d[0])
+		}
+		h = d[:l-1]
+	}
+	return
+}
+
+//adjust heap to select a proper hole to set v
+func (this *GOGPHeapNamePrefixHeap) adjust(d []GOGPHeapElem, hole int, v GOGPHeapElem) {}
+
+//make d as h heap
+func (this *GOGPHeapNamePrefixHeap) MakeHeap(d []GOGPHeapElem) {
+	if l := len(d); l > 1 {
+		for i := 0; i < l; i++ {
+			this.PushHeap(d[:i], d[i])
+		}
+	}
+}
+
+//reverse order of d
+func (this *GOGPHeapNamePrefixHeap) Reverse(d []GOGPHeapElem) {
+	l := len(d) - 1
+	m := l >> 1
+	for i := 0; i <= m; i++ {
+		d[i], d[l-i] = d[l-i], d[i]
+	}
+}
+
+//sort slice use heap algorithm
+func (this *GOGPHeapNamePrefixHeap) SortHeap(d []GOGPHeapElem) []GOGPHeapElem {
+	this.MakeHeap(d)
+	for t := d; len(t) > 1; {
+		t, _, _ = this.PopHeap(t)
+	}
+	return d
 }
 
 //new object
-func NewGOGPHeapNamePrefixHeap(capacity int) (r *GOGPHeapNamePrefixHeap) {
+func NewGOGPHeapNamePrefixHeap(capacity int, maxTop bool) (r *GOGPHeapNamePrefixHeap) {
 	r = &GOGPHeapNamePrefixHeap{}
-	r.Init(capacity, 0, false)
+	r.Init(capacity, 0, maxTop)
 	return
 }
 
 //initialize
-func (this *GOGPHeapNamePrefixHeap) Init(capacity, limitN int, max bool) {
+func (this *GOGPHeapNamePrefixHeap) Init(capacity, limitN int, maxTop bool) {
 	if cap(this.d) != capacity {
 		this.d = make([]GOGPHeapElem, 0, capacity)
 	}
 	this.d = this.d[:0]
 
 	this.limitN = limitN
-	this.maxTop = max
+	this.maxTop = maxTop
+}
+
+//heap slice
+func (this *GOGPHeapNamePrefixHeap) D() []GOGPHeapElem {
+	return this.d
 }
 
 //push
 func (this *GOGPHeapNamePrefixHeap) Push(v GOGPHeapElem) {
-	this.d = append(this.d, v)
-	idx := this.Size() - 1
-	parent := this.parent(idx)
-	for ; idx > 0; idx, parent = parent, this.parent(parent) {
-		if this.cmp(idx, parent) {
-			this.swap(idx, parent)
-		} else {
-			break
+	if this.limitN > 0 && this.Size() >= this.limitN {
+		if top, ok := this.Top(); ok && this.cmpV(top, v) {
+			this.Pop()
 		}
 	}
+	this.d = this.PushHeap(this.d, v)
 }
 
 //pop
 func (this *GOGPHeapNamePrefixHeap) Pop() (top GOGPHeapElem, ok bool) {
-	if top, ok = this.Top(); ok {
-		this.swap(0, this.Size()-1)
-		for i, l, r := 0, 1, 2; ; l, r = this.lchild(i), this.rchild(i) {
-			if l == r {
-			}
-		}
-	}
+	this.d, top, ok = this.PopHeap(this.d)
 	return
 }
 
+//heap top
 func (this *GOGPHeapNamePrefixHeap) Top() (top GOGPHeapElem, ok bool) {
 	if ok = !this.Empty(); ok {
 		top = this.d[0]
@@ -96,36 +151,45 @@ func (this *GOGPHeapNamePrefixHeap) Top() (top GOGPHeapElem, ok bool) {
 	return
 }
 
-func (this *GOGPHeapNamePrefixHeap) cmp(i, j int) (ok bool) {
-	l, r := this.d[i], this.d[j]
+//cmpare value
+func (this *GOGPHeapNamePrefixHeap) cmpV(c, p GOGPHeapElem) (ok bool) {
+	//#if GOGP_HasLess==true
 	if this.maxTop {
-		//#if GOGP_HasLess
-		ok = r.Less(l)
-		//#else
-		ok = r < l
-		//#endif
+		ok = !p.Less(c)
 	} else {
-		//#if GOGP_HasLess
-		ok = l.Less(r)
-		//#else
-		ok = l < r
-		//#endif
+		ok = !c.Less(p)
 	}
-
+	//#else
+	if this.maxTop {
+		ok = !(p < c)
+	} else {
+		ok = !(c < p)
+	}
+	//#endif
 	return
 }
-func (this *GOGPHeapNamePrefixHeap) swap(i, j int) {
-	this.d[i], this.d[j] = this.d[j], this.d[i]
-}
 
+//compare index
+//func (this *GOGPHeapNamePrefixHeap) cmp(c, p int) (ok bool) {
+//	this.cmpV(this.d[c], this.d[p])
+//	return
+//}
+
+//func (this *GOGPHeapNamePrefixHeap) swap(i, j int) {
+//	this.d[i], this.d[j] = this.d[j], this.d[i]
+//}
+
+//get parent index
 func (this *GOGPHeapNamePrefixHeap) parent(idx int) int {
 	return (idx - 1) / 2
 }
 
+//get left child index
 func (this *GOGPHeapNamePrefixHeap) lchild(idx int) int {
 	return 2*idx + 1
 }
 
+//get right child index
 func (this *GOGPHeapNamePrefixHeap) rchild(idx int) int {
 	return 2*idx + 2
 }
@@ -135,11 +199,10 @@ func (this *GOGPHeapNamePrefixHeap) Size() int {
 	return len(this.d)
 }
 
+//empty
 func (this *GOGPHeapNamePrefixHeap) Empty() bool {
 	return this.Size() == 0
 }
-
-func (this *GOGPHeapNamePrefixHeap) MakeHeap() {}
 
 //GOGP_IGNORE_BEGIN//////////////////////////////GOGPCommentDummyGoFile
 //*/
