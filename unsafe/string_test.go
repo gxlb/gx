0a1ebf4ee3fe50d74c82c13d2c57ebc8496fc249
Ally Dale <vipally@163.com>
2016-10-26 23:16:13 +0800

    test case

--- unsafe/string_test.go
+++ unsafe/string_test.go
@@ -7,6 +7,22 @@ import (
 	"github.com/vipally/gx/unsafe"
 )
 
+//test result
+//String to bytes:
+//0	s=     	ptr(v)=0x51bd70    	ptr(StringBytes(v)=0x51bd70    	ptr([]byte(v)=0xc042021c58
+//1	s=     	ptr(v)=0x51bd70    	ptr(StringBytes(v)=0x51bd70    	ptr([]byte(v)=0xc042021c58
+//2	s=hello	ptr(v)=0x51c2fa    	ptr(StringBytes(v)=0x51c2fa    	ptr([]byte(v)=0xc042021c58
+//3	s=hello	ptr(v)=0x51c2fa    	ptr(StringBytes(v)=0x51c2fa    	ptr([]byte(v)=0xc042021c58
+//4	s=     	ptr(v)=<nil>       	ptr(StringBytes(v)=<nil>       	ptr([]byte(v)=0xc042021c58
+//5	s=     	ptr(v)=<nil>       	ptr(StringBytes(v)=<nil>       	ptr([]byte(v)=0xc042021c58
+//6	s=xello	ptr(v)=0xc0420444b5	ptr(StringBytes(v)=0xc0420444b5	ptr([]byte(v)=0xc042021c58
+//7	s=xello	ptr(v)=0xc0420444ba	ptr(StringBytes(v)=0xc0420444ba	ptr([]byte(v)=0xc042021c58
+//Bytes to string:
+//0	s=     	ptr(v)=0x5c38b8    	ptr(StringBytes(v)=0x5c38b8    	ptr(string(v)=<nil>
+//1	s=hello	ptr(v)=0xc0420445e0	ptr(StringBytes(v)=0xc0420445e0	ptr(string(v)=0xc042021c38
+//Benchmark_Normal-4   	1000000000	         0.87 ns/op
+//Benchmark_Direct-4   	2000000000	         0.24 ns/op
+
 func TestPointer(t *testing.T) {
 	s := []string{
 		"",
@@ -39,23 +55,8 @@ func TestPointer(t *testing.T) {
 		s2 := string(v)
 		fmt.Printf("%d\ts=%5s\tptr(v)=%-12v\tptr(StringBytes(v)=%-12v\tptr(string(v)=%-12v\n",
 			i, s1, unsafe.BytesPointer(v), s1.Pointer(), unsafe.StringPointer(s2))
-
 	}
 
-	//String to bytes:
-	//0	s=     	ptr(v)=0x51bd70    	ptr(StringBytes(v)=0x51bd70    	ptr([]byte(v)=0xc042021c58
-	//1	s=     	ptr(v)=0x51bd70    	ptr(StringBytes(v)=0x51bd70    	ptr([]byte(v)=0xc042021c58
-	//2	s=hello	ptr(v)=0x51c2fa    	ptr(StringBytes(v)=0x51c2fa    	ptr([]byte(v)=0xc042021c58
-	//3	s=hello	ptr(v)=0x51c2fa    	ptr(StringBytes(v)=0x51c2fa    	ptr([]byte(v)=0xc042021c58
-	//4	s=     	ptr(v)=<nil>       	ptr(StringBytes(v)=<nil>       	ptr([]byte(v)=0xc042021c58
-	//5	s=     	ptr(v)=<nil>       	ptr(StringBytes(v)=<nil>       	ptr([]byte(v)=0xc042021c58
-	//6	s=xello	ptr(v)=0xc0420444b5	ptr(StringBytes(v)=0xc0420444b5	ptr([]byte(v)=0xc042021c58
-	//7	s=xello	ptr(v)=0xc0420444ba	ptr(StringBytes(v)=0xc0420444ba	ptr([]byte(v)=0xc042021c58
-	//Bytes to string:
-	//0	s=     	ptr(v)=0x5c38b8    	ptr(StringBytes(v)=0x5c38b8    	ptr(string(v)=<nil>
-	//1	s=hello	ptr(v)=0xc0420445e0	ptr(StringBytes(v)=0xc0420445e0	ptr(string(v)=0xc042021c38
-	//Benchmark_Normal-4   	1000000000	         0.87 ns/op
-	//Benchmark_Direct-4   	2000000000	         0.24 ns/op
 }
 
 const N = 3000000
