3455519cb758a33f39dce9a0ead63c6b1616bce7
Ally Dale <vipally@163.com>
2016-10-26 11:37:02 +0800

    unsafe

--- unsafe/string.go
+++ unsafe/string.go
@@ -1,13 +1,35 @@
 package unsafe
 
 import (
-	//"reflect"
+	"reflect"
 	"unsafe"
 )
 
+type StringBytesT []byte
+
+//how to do this?
+func (this *StringBytesT) Writeable() bool {
+	return false
+}
+
 //return GoString's buffer slice(enable modify string)
-func StringBufer(s string) []byte {
-	//b := (*[]byte(*reflect.SliceHeader(*reflect.StringHeader(unsafe.Pointer(&s)))))
-	b := (*[]byte)(unsafe.Pointer(&s))
-	return *b
+func StringBytes(s string) StringBytesT {
+	return *(*StringBytesT)(unsafe.Pointer(&s))
+}
+
+// convert b to string without copy
+func BytesString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// returns &s[0], which is not allowed in go
+func StringPointer(s string) unsafe.Pointer {
+	p := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	return unsafe.Pointer(p.Data)
+}
+
+// returns &b[0], which is not allowed in go
+func BytesPointer(b []byte) unsafe.Pointer {
+	p := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	return unsafe.Pointer(p.Data)
 }
--- unsafe/string_test.go
+++ unsafe/string_test.go
@@ -8,8 +8,19 @@ import (
 )
 
 func TestString(t *testing.T) {
-	s := "hello"
-	b := unsafe.StringBufer(s)
-	b[0] = 'G'
-	fmt.Println(s)
+	s := ""
+	var bb []byte
+	fmt.Println(bb)
+	fmt.Println(unsafe.BytesPointer(bb))
+	fmt.Println(unsafe.StringBytes(s))
+	//s = "hello"
+
+	b := []byte{117, 118}
+	s2 := fmt.Sprintf("hhh")
+	bs := unsafe.StringBytes(s2)
+	fmt.Println(unsafe.StringPointer(s))
+	fmt.Println(unsafe.StringPointer(s2))
+	fmt.Println(unsafe.BytesPointer(b))
+	bs[0] = 'a'
+	fmt.Println(string(bs))
 }
