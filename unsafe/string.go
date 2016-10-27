e971fed5a7eb30d5a298081cef3d32176636cd50
Ally Dale <vipally@163.com>
2016-10-27 11:21:16 +0800

    unsafe

--- unsafe/string.go
+++ unsafe/string.go
@@ -7,29 +7,40 @@ import (
 
 var (
 	//dynamic data address
-	firstPointer = uintptr(unsafe.Pointer(new(int))) >> 7
+	firstPointer = uintptr(unsafe.Pointer(new(int)))
 )
 
+type Pointer unsafe.Pointer
 type Bytes []byte
 type String string
+
+//func (me Pointer) Writeable() bool {
+//	addr := uintptr(me)
+//	return addr >= firstPointer
+//}
+
+func Writeable(ptr Pointer) bool {
+	addr := uintptr(ptr)
+	return addr >= firstPointer
+}
 
 //how to do this?
 func (me Bytes) Writeable() bool {
 	addr := uintptr(BytesPointer(me))
-	return addr > firstPointer
+	return addr >= firstPointer
 }
 
-func (me Bytes) Pointer() unsafe.Pointer {
+func (me Bytes) Pointer() Pointer {
 	return BytesPointer(me)
 }
 
 //check if a string's buffer is writeable
 func (me String) Writeable() bool {
 	p := uintptr(StringPointer(string(me)))
-	return p > firstPointer
+	return p >= firstPointer
 }
 
-func (me String) Pointer() unsafe.Pointer {
+func (me String) Pointer() Pointer {
 	return StringPointer(string(me))
 }
 
@@ -44,13 +55,13 @@ func BytesString(b []byte) String {
 }
 
 // returns &s[0], which is not allowed in go
-func StringPointer(s string) unsafe.Pointer {
+func StringPointer(s string) Pointer {
 	p := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	return unsafe.Pointer(p.Data)
+	return Pointer(p.Data)
 }
 
 // returns &b[0], which is not allowed in go
-func BytesPointer(b []byte) unsafe.Pointer {
+func BytesPointer(b []byte) Pointer {
 	p := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	return unsafe.Pointer(p.Data)
+	return Pointer(p.Data)
 }
