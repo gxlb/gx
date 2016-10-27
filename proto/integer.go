50982017b5b2a332233df643b4135820424c930f
Ally Dale <vipally@163.com>
2016-10-27 19:07:48 +0800

    integer

---
+++ proto/integer.go
@@ -0,0 +1,34 @@
+type Integer int64
+type UInteger uint64
+
+func (me Integer) Int64() int64 {
+	return int64(me)
+}
+
+func (me Integer) Int32() int64 {
+	return int32(me)
+}
+
+func (me Integer) Int16() int64 {
+	return int16(me)
+}
+
+func (me Integer) Int8() int64 {
+	return int8(me)
+}
+
+func (me UInteger) Uint64() int64 {
+	return int64(me)
+}
+
+func (me Uinteger) Uint32() int64 {
+	return int32(me)
+}
+
+func (me Uinteger) Uint16() int64 {
+	return int16(me)
+}
+
+func (me Uinteger) Uint8() int64 {
+	return int8(me)
+}
