a34de9a336aee632845b14d9a65241f03476b99c
Ally Dale <vipally@163.com>
2016-10-28 16:55:30 +0800

    bitset

--- stl/bit_set.go
+++ stl/bit_set.go
@@ -1,1 +1,17 @@
 package stl
+
+//s set of bit for vistit
+type BitSet struct {
+	d []uint
+}
+
+//func (this *BitSet) Bits(start, end uint) *Bits {}
+
+//operator of bits
+type Bits struct {
+	start, end uint8
+	d          *BitSet
+}
+
+//func (this *Bits) Get() uint       {}
+//func (this *Bits) Set(v uint) uint {}
