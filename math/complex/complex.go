5aabbe41ae3c167a79bceeb69afac25599f1f284
Ally Dale <vipally@163.com>
2016-10-28 17:50:01 +0800

    complex

---
+++ math/complex/complex.go
@@ -0,0 +1,19 @@
+package complex
+
+type Cplx64 complex64
+
+func (this *Cplx64) Real() float32 {
+	return real(*this)
+}
+func (this *Cplx64) Imag() float32 {
+	return imag(*this)
+}
+
+type Cplx128 complex128
+
+func (this *Cplx128) Real() float64 {
+	return real(*this)
+}
+func (this *Cplx128) Imag() float64 {
+	return imag(*this)
+}
