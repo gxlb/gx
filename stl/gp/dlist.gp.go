27b9159c9c3e045b4484cf2b2d23b3290d2c1d37
Ally Dale <vipally@163.com>
2016-11-13 23:15:09 +0800

    list

--- stl/gp/dlist.gp.go
+++ stl/gp/dlist.gp.go
@@ -15,18 +15,8 @@ package gp
 //
 //#GOGP_IGNORE_END////////////////////////////////GOGPCommentDummyGoFile
 
-//import here...
-
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPDummyDefine
-//
-//these defines is used to make sure this dummy go file can be compiled correctlly
-//and they will be removed from real go files
-//
-
-type GOGPDListData int
-
-//
-//#GOGP_IGNORE_END////////////////////////////////GOGPDummyDefine
+//#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/fakedef,_)
+//#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/functorcmp)
 
 //list node
 type GOGPListNamePrefixDListNode struct {
--- stl/gp/slist.gp.go
+++ stl/gp/slist.gp.go
@@ -15,44 +15,149 @@ package gp
 //
 //#GOGP_IGNORE_END////////////////////////////////GOGPCommentDummyGoFile
 
-//import here...
-
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPDummyDefine
-//
-//these defines is used to make sure this dummy go file can be compiled correctlly
+//#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/fakedef,_)
+//#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gx/stl/gp/fakedef)
+//these defines is used to make sure this fake go file can be compiled correctlly
 //and they will be removed from real go files
-//
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-type GOGPSListData int
+type GOGPValueType int                               //
+func (this GOGPValueType) Less(o GOGPValueType) bool { return this < o }
+func (this GOGPValueType) Show() string              { return "" } //
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//#GOGP_IGNORE_END //required from(github.com/vipally/gx/stl/gp/fakedef)
 
-//
-//#GOGP_IGNORE_END////////////////////////////////GOGPDummyDefine
+//#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/functorcmp)
+//#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gx/stl/gp/functorcmp)
+//this file is used to //import by other gp files
+//it cannot use independently, simulation C++ stl functors
+//package gp
 
-//list node
-type GOGPListNamePrefixSListNode struct {
-	GOGPSListData
-	next *GOGPListNamePrefixSListNode
+const (
+	CMPLesser = iota //default
+	CMPGreater
+) //
+
+//cmp object, zero is Lesser
+type CmpGOGPGlobalNamePrefix byte
+
+const (
+	CmpGOGPGlobalNamePrefixLesser  CmpGOGPGlobalNamePrefix = CMPLesser
+	CmpGOGPGlobalNamePrefixGreater CmpGOGPGlobalNamePrefix = CMPGreater
+)
+
+//create cmp object by name
+func CreateCmpGOGPGlobalNamePrefix(cmpName string) (r CmpGOGPGlobalNamePrefix) {
+	r = CmpGOGPGlobalNamePrefixLesser.CreateByName(cmpName)
+	return
 }
 
-func (this *GOGPListNamePrefixSListNode) Get() *GOGPSListData {
-	return &this.GOGPSListData
+//uniformed global function
+func (me CmpGOGPGlobalNamePrefix) F(left, right GOGPValueType) (ok bool) {
+	switch me {
+	case CMPLesser:
+		ok = me.less(left, right)
+	case CMPGreater:
+		ok = me.great(left, right)
+	}
+	return
+}
+
+//Lesser object
+func (me CmpGOGPGlobalNamePrefix) Lesser() CmpGOGPGlobalNamePrefix { return CMPLesser }
+
+//Greater object
+func (me CmpGOGPGlobalNamePrefix) Greater() CmpGOGPGlobalNamePrefix { return CMPGreater }
+
+//show as string
+func (me CmpGOGPGlobalNamePrefix) String() (s string) {
+	switch me {
+	case CMPLesser:
+		s = "Lesser"
+	case CMPGreater:
+		s = "Greater"
+	default:
+		s = "error cmp value"
+	}
+	return
+}
+
+//create by bool
+func (me CmpGOGPGlobalNamePrefix) CreateByBool(bigFirst bool) (r CmpGOGPGlobalNamePrefix) {
+	if bigFirst {
+		r = CMPGreater
+	} else {
+		r = CMPLesser
+	}
+	return
+}
+
+//create cmp object by name
+func (me CmpGOGPGlobalNamePrefix) CreateByName(cmpName string) (r CmpGOGPGlobalNamePrefix) {
+	switch cmpName {
+	case "": //default Lesser
+		fallthrough
+	case "Lesser":
+		r = CMPLesser
+	case "Greater":
+		r = CMPGreater
+	default: //unsupport name
+		panic(cmpName)
+	}
+	return
+}
+
+//lesser operation
+func (me CmpGOGPGlobalNamePrefix) less(left, right GOGPValueType) (ok bool) {
+
+	ok = left < right
+
+	return
+}
+
+//Greater operation
+func (me CmpGOGPGlobalNamePrefix) great(left, right GOGPValueType) (ok bool) {
+
+	ok = right < left
+
+	return
+}
+
+//#GOGP_IGNORE_END //required from(github.com/vipally/gx/stl/gp/functorcmp)
+
+var gGOGPGlobalNamePrefixSListGbl struct {
+	cmp CmpGOGPGlobalNamePrefix
+}
+
+func init() {
+	gGOGPGlobalNamePrefixSListGbl.cmp = gGOGPGlobalNamePrefixSListGbl.cmp.CreateByName("#GOGP_GPGCFG(GOGP_DefaultCmpType)")
+}
+
+//list node
+type GOGPGlobalNamePrefixSListNode struct {
+	GOGPValueType
+	next *GOGPGlobalNamePrefixSListNode
+}
+
+func (this *GOGPGlobalNamePrefixSListNode) Get() *GOGPValueType {
+	return &this.GOGPValueType
 }
 
 type GOGPTreeNamePrefixRBTreeNodeVisitor struct {
-	node, head *GOGPListNamePrefixSListNode
+	node, head *GOGPGlobalNamePrefixSListNode
 }
 
 func (this *GOGPTreeNamePrefixRBTreeNodeVisitor) Next() bool {
 	return false
 }
 
-func (this *GOGPTreeNamePrefixRBTreeNodeVisitor) Node() *GOGPListNamePrefixSListNode {
+func (this *GOGPTreeNamePrefixRBTreeNodeVisitor) Node() *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
 
 //list object
 type GOGPListNamePrefixSList struct {
-	head, tail *GOGPListNamePrefixSListNode
+	head, tail *GOGPGlobalNamePrefixSListNode
 }
 
 //new object
@@ -63,22 +168,22 @@ func NewGOGPStackNamePrefixSList() *GOGPListNamePrefixSList {
 func (this *GOGPListNamePrefixSList) Len() int {
 	return 0
 }
-func (this *GOGPListNamePrefixSList) Visitor(node *GOGPListNamePrefixSListNode) *GOGPTreeNamePrefixRBTreeNodeVisitor {
+func (this *GOGPListNamePrefixSList) Visitor(node *GOGPGlobalNamePrefixSListNode) *GOGPTreeNamePrefixRBTreeNodeVisitor {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) Front() *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) Front() *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) Back() *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) Back() *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) Clear() *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) Clear() *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) PushFront(v GOGPSListData) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) PushFront(v GOGPValueType) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) PushBack(v GOGPSListData) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) PushBack(v GOGPValueType) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
 func (this *GOGPListNamePrefixSList) PushFrontList(other *GOGPListNamePrefixSList) {
@@ -88,25 +193,25 @@ func (this *GOGPListNamePrefixSList) PushBackList(other *GOGPListNamePrefixSList
 	return
 }
 
-func (this *GOGPListNamePrefixSList) InsertBefore(v GOGPSListData, mark *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) InsertBefore(v GOGPValueType, mark *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) InsertAfter(v GOGPSListData, mark *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) InsertAfter(v GOGPValueType, mark *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) Remove(node *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) Remove(node *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) MoveFront(node *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) MoveFront(node *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) MoveBck(node *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) MoveBck(node *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) MoveBefore(node, mark *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) MoveBefore(node, mark *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
-func (this *GOGPListNamePrefixSList) MoveAfter(node, mark *GOGPListNamePrefixSListNode) *GOGPListNamePrefixSListNode {
+func (this *GOGPListNamePrefixSList) MoveAfter(node, mark *GOGPGlobalNamePrefixSListNode) *GOGPGlobalNamePrefixSListNode {
 	return nil
 }
 func (this *GOGPListNamePrefixSList) Sort() {
