329a2d4e257c9be9eca1ec56e833f51b9c1d000b
Ally Dale <vipally@163.com>
2016-11-21 22:56:00 +0800

    init fake go files

--- stl/gp/bstree.gp.go
+++ stl/gp/bstree.gp.go
@@ -1,20 +1,6 @@
 package gp
 
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPCommentDummyGoFile_BEGIN
-//
-//
-/*   //<----This line can be uncommented to disable all this file, and it doesn't effect to the .gp file
-//	 //If test or change .gp file required, comment it to modify and cmomile as normal go file
-//
-//
-// This is exactly not a real go code file
-// It is used to generate .gp file by gogp tool
-// Real go code file will be generated from .gp file
-//
-//#GOGP_IGNORE_END////////////////////////////////GOGPCommentDummyGoFile
-
-//import here...
-//
+//#GOGP_FILE_BEGIN
 
 //#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/fakedef,_)
 //#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gx/stl/gp/fakedef)
@@ -27,8 +13,6 @@ func (this GOGPValueType) Less(o GOGPValueType) bool { return this < o }
 func (this GOGPValueType) Show() string              { return "" } //
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //#GOGP_IGNORE_END //required from(github.com/vipally/gx/stl/gp/fakedef)
-
-
 
 //#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/functorcmp)
 //#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gx/stl/gp/functorcmp)
@@ -130,16 +114,7 @@ func (me CmpGOGPGlobalNamePart) great(left, right GOGPValueType) (ok bool) {
 
 //#GOGP_IGNORE_END //required from(github.com/vipally/gx/stl/gp/functorcmp)
 
-////#GOGP_IGNORE_BEGIN//////////////////////////////GOGPDummyDefine
-//
-//these defines is used to make sure this dummy go file can be compiled correctlly
-//and they will be removed from real go files
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-//
-//
-//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-//#GOGP_IGNORE_END////////////////////////////////GOGPDummyDefine
+////////////////////////////////////////////////////////////////////////////////
 
 type GOGPGlobalNamePrefixBSTree struct {
 	root *GOGPGlobalNamePrefixTreeNode
@@ -147,11 +122,4 @@ type GOGPGlobalNamePrefixBSTree struct {
 
 type GOGPGlobalNamePrefixBSTreeNode struct{}
 
-//
-//add file body here..........
-//
-//
-
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPCommentDummyGoFile
-//*/
-//#GOGP_IGNORE_END////////////////////////////////GOGPCommentDummyGoFile_END
+//#GOGP_FILE_END
