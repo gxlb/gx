ad7ae7044e5f19096894ff1d699eda3b80e16bf4
Ally Dale <vipally@163.com>
2017-02-25 21:59:24 +0800

    debug

---
+++ debug/log.go
@@ -0,0 +1,3 @@
+package debug
+
+func Log() {}
---
+++ debug/switch.go
@@ -0,0 +1,5 @@
+package degug
+
+func GetSwitch(nId int) bool             { return false }
+func SetSwitch(nId int, state bool) bool { return false }
+func RegSwitch(nId int) bool             { return false }
