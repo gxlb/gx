59f2b846d5a8cd0d432ec9f356dd4869d7087f11
Ally Dale <vipally@163.com>
2017-03-16 23:40:03 +0800

    update products

--- regable/gp/mapping.gp.go
+++ regable/gp/mapping.gp.go
@@ -1,17 +1,47 @@
 package gp
 
 //#GOGP_FILE_BEGIN
+//#GOGP_IGNORE_BEGIN ///gogp_file_begin
+//
+/*   //This line can be uncommented to disable all this file, and it doesn't effect to the .gp file
+//	 //If test or change .gp file required, comment it to modify and cmomile as normal go file
+//
+// This is a fake go code file
+// It is used to generate .gp file by gogp tool
+// Real go code file will be generated from .gp file
+//
+//#GOGP_IGNORE_END ///gogp_file_begin
 
 import (
 	"fmt"
 )
 
 //#GOGP_REQUIRE(github.com/vipally/gogp/lib/fakedef,_)
+//#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gogp/lib/fakedef)
+//these defines are used to make sure this fake go file can be compiled correctlly
+//and they will be removed from real go files
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+type GOGPKeyType int                             //
+func (this GOGPKeyType) Less(o GOGPKeyType) bool { return this < o }
+func (this GOGPKeyType) Show() string            { return "" } //
+
+type GOGPValueType int                               //
+func (this GOGPValueType) Less(o GOGPValueType) bool { return this < o }
+func (this GOGPValueType) Show() string              { return "" } //
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//#GOGP_IGNORE_END //required from(github.com/vipally/gogp/lib/fakedef)
 
 //key/value mapping
 type GOGPGlobalNamePrefixMapping struct {
 	normal  map[GOGPKeyType]GOGPValueType
 	reverse map[GOGPValueType]GOGPKeyType
+}
+
+func NewGOGPGlobalNamePrefixMapping() *GOGPGlobalNamePrefixMapping {
+	p := &GOGPGlobalNamePrefixMapping{}
+	p.Init()
+	return p
 }
 
 func (this *GOGPGlobalNamePrefixMapping) Init() {
@@ -64,4 +94,12 @@ func (this *GOGPGlobalNamePrefixMapping) ReverseFind(v GOGPValueType) (k GOGPKey
 	return
 }
 
+//clear all mapping
+func (this *GOGPGlobalNamePrefixMapping) Clear() {
+	this.Init()
+}
+
 //#GOGP_FILE_END
+//#GOGP_IGNORE_BEGIN ///gogp_file_end
+//*/
+//#GOGP_IGNORE_END ///gogp_file_end
