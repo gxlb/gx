575595d6ca24585fdc2ccac55ce1e69324aec9e1
Ally Dale <vipally@163.com>
2017-03-17 23:19:44 +0800

    regable

---
+++ regable/gp/factory.gp.go
@@ -0,0 +1,43 @@
+package gp
+
+//#GOGP_FILE_BEGIN
+//#GOGP_REQUIRE(github.com/vipally/gogp/lib/fakedef,_)
+type Product interface{}
+type GOGPParaType int
+type GOGPIdType uint32
+
+type Creater interface {
+	Create(GOGPPara GOGPParaType) Product
+	Name() string
+	Id() GOGPIdType
+}
+
+type GOGPGlobalNamePrefixFactory struct {
+	lock bool //if the factory is lock for regist
+}
+
+func (this *GOGPGlobalNamePrefixFactory) RegCreater(creater Creater) error {
+	return nil
+}
+
+func (this *GOGPGlobalNamePrefixFactory) Lock() {
+	this.lock = true
+}
+
+func (this *GOGPGlobalNamePrefixFactory) GetCreaterById(id GOGPIdType) (creater Creater, ok bool) {
+	return
+}
+
+func (this *GOGPGlobalNamePrefixFactory) GetCreaterByName(name string) (creater Creater, ok bool) {
+	return
+}
+
+func (this *GOGPGlobalNamePrefixFactory) CreateById(id GOGPIdType) (product Product, ok bool) {
+	return
+}
+
+func (this *GOGPGlobalNamePrefixFactory) CreateByName(name string) (product Product, ok bool) {
+	return
+}
+
+//#GOGP_FILE_END
---
+++ regable/gp/regist.gp.go
@@ -0,0 +1,7 @@
+package gp
+
+//#GOGP_FILE_BEGIN
+
+//#GOGP_REQUIRE(github.com/vipally/gogp/lib/fakedef,_)
+
+//#GOGP_FILE_END
