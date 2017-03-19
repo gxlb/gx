07173e2ac98adac598a68b957b390ba314d13ecc
Ally Dale <vipally@163.com>
2017-03-19 23:48:12 +0800

    regable

--- regable/gp/regist.gp.go
+++ regable/gp/regist.gp.go
@@ -3,5 +3,27 @@ package gp
 //#GOGP_FILE_BEGIN
 
 //#GOGP_REQUIRE(github.com/vipally/gogp/lib/fakedef,_)
+type GOGPValueType int
+
+func NewGOGPGlobalNamePrefixReger(name string) (*GOGPGlobalNamePrefixReger, error) {
+	return nil, nil
+}
+
+type GOGPGlobalNamePrefixReger struct {
+	regerId  uint8
+	name     string
+	subIdGen uint32
+	regList  []*_GOGPGlobalNamePrefixRecord
+}
+
+type _GOGPGlobalNamePrefixRecord struct {
+	//name string
+	val GOGPValueType
+	id  uint32
+}
+
+type GOGPGlobalNamePrefixRegerManager struct {
+	list []*GOGPGlobalNamePrefixReger
+}
 
 //#GOGP_FILE_END
