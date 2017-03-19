2351ea8ab78e946a4dbd3635f13d1935872a5af7
Ally Dale <vipally@163.com>
2017-03-19 16:52:58 +0800

    factory

--- factory/factory.go
+++ factory/factory.go
@@ -1,26 +1,20 @@
-//对象工厂 每个类型都包含一个Creator 对应的对象由相应的Creater生产 出来的对象都用interface{}表示
-//有了这个对象工厂 就可以通过配置预定义好的字符串 生成对应的对象了
-//像协议buff转协议对象这种活就都由这个factory承包了
-
-//实现工厂模式
 package factory
 
-type Creater interface{ //Factory
-	Create(arg... interface{}) interface{}
+type Creater interface { //Factory
+	Create(arg ...interface{}) interface{}
 }
 
-var(
+var (
 	g_factory = NewFactoryS()
 )
 
-
-func AddCreater(typename string, creater Creater) error{
+func AddCreater(typename string, creater Creater) error {
 	return g_factory.AddCreater(typename, creater)
 }
-func RemoveCreater(typename string) error{
+func RemoveCreater(typename string) error {
 	return g_factory.RemoveCreater(typename)
 }
 
-func Create(typename string, arg... interface{}) interface{}{
+func Create(typename string, arg ...interface{}) interface{} {
 	return g_factory.Create(typename, arg...)
 }
