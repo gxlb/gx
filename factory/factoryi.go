7d4cca527c6e12ac27d4caa17f3c570e038a5983
Ally Dale <vipally@163.com>
2017-03-19 16:50:41 +0800

    factory

--- factory/factoryi.go
+++ factory/factoryi.go
@@ -1,22 +1,23 @@
 package factory
 
-func NewFactoryI() *FactoryI{
-	return &FactoryI{cmap: make(map[int] Creater)}
+func NewFactoryI() *FactoryI {
+	return &FactoryI{cmap: make(map[int]Creater)}
 }
-//int作为类型标识的工厂
-type FactoryI struct{ 
-	cmap map[int] Creater
+
+type FactoryI struct {
+	cmap map[int]Creater
 }
-func (me *FactoryI)AddCreater(nType int, creater Creater) error{
+
+func (me *FactoryI) AddCreater(nType int, creater Creater) error {
 	return nil
 }
 
-func (me *FactoryI)RemoveCreater(nType int) error{
+func (me *FactoryI) RemoveCreater(nType int) error {
 	return nil
 }
 
-func (me *FactoryI)Create(nType int, arg... interface{}) interface{}{
-	if p,ok:=me.cmap[nType];ok{
+func (me *FactoryI) Create(nType int, arg ...interface{}) interface{} {
+	if p, ok := me.cmap[nType]; ok {
 		return p.Create(arg...)
 	}
 	return nil
