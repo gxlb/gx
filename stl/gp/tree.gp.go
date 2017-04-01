3a154d3bf57348fd50c732aeda0320bacd282841
Ally Dale <vipally@gmail.com>
2017-04-01 22:49:14 +0800

    update

--- stl/gp/tree.gp.go
+++ stl/gp/tree.gp.go
@@ -18,7 +18,7 @@ package gp
 import "sort" //#GOGP_IGNORE_END
 
 //#GOGP_REQUIRE(github.com/vipally/gogp/lib/fakedef,_)
-//#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gogp/lib/fakedef)
+//#GOGP_IGNORE_BEGIN ///require begin from(github.com/vipally/gogp/lib/fakedef)
 //these defines are used to make sure this fake go file can be compiled correctlly
 //and they will be removed from real go files
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
@@ -27,10 +27,10 @@ type GOGPValueType int                               //
 func (this GOGPValueType) Less(o GOGPValueType) bool { return this < o }
 func (this GOGPValueType) Show() string              { return "" } //
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-//#GOGP_IGNORE_END //required from(github.com/vipally/gogp/lib/fakedef)
+//#GOGP_IGNORE_END ///require end from(github.com/vipally/gogp/lib/fakedef)
 
 //#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/functorcmp,#GOGP_GPGCFG(GOGP_SectionSortSlice))
-//#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gx/stl/gp/functorcmp)
+//#GOGP_IGNORE_BEGIN ///require begin from(github.com/vipally/gx/stl/gp/functorcmp)
 //this file is used to //import by other gp files
 //it cannot use independently, simulation C++ stl functors
 
@@ -126,10 +126,10 @@ func (me CmpGOGPGlobalNamePrefixTreeNode) great(left, right *GOGPGlobalNamePrefi
 	return
 }
 
-//#GOGP_IGNORE_END //required from(github.com/vipally/gx/stl/gp/functorcmp)
+//#GOGP_IGNORE_END ///require end from(github.com/vipally/gx/stl/gp/functorcmp)
 
 //#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/sort_slice,#GOGP_GPGCFG(GOGP_SectionSortSlice))
-//#GOGP_IGNORE_BEGIN //required from(github.com/vipally/gx/stl/gp/sort_slice)
+//#GOGP_IGNORE_BEGIN ///require begin from(github.com/vipally/gx/stl/gp/sort_slice)
 //this file define a template type for sort
 
 //package gp
@@ -242,7 +242,7 @@ func (this *GOGPGlobalNamePrefixTreeNodeSortSlice) Swap(i, j int) {
 	(this.d)[i], (this.d)[j] = (this.d)[j], (this.d)[i]
 }
 
-//#GOGP_IGNORE_END //required from(github.com/vipally/gx/stl/gp/sort_slice)
+//#GOGP_IGNORE_END ///require end from(github.com/vipally/gx/stl/gp/sort_slice)
 
 ////////////////////////////////////////////////////////////////////////////////
 
